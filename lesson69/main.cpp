b05e89a131722558257c7067ddafe8ca30e3360c
typsdkvmj <dasha.all555@gmail.com>
2023-05-30 18:44:04 +0300

    change subclasses

--- lesson69/main.cpp
+++ lesson69/main.cpp
@@ -1,22 +1,24 @@
 #include "gasStation.h"
+#include "car.h"
+#include "bus.h"
+#include "truck.h"
 
 int main(void) {
-	Car car1(50, 200, 1600);
-	Car car2(60, 240, 2000);
-	Car car3(40, 160, 1200);
+	Car car1();
+	Car* car2 = new Car();
 
-	Bus bus(200, 50);
+	Transport* transport = new Transport();
+	delete transport;
 
-	Truck truck1(200, 20000);
-	Truck truck2(350, 25000);
+	transport = new Car();
+	delete transport;
 
-	Car cars[3] = { car1,car2,car3 };
-	Bus buses[1] = { bus };
-	Truck trucks[2] = { truck1, truck2 };
+	transport = new Bus();
+	delete transport;
 
-	int total = GasStation::calculateTotalGas(buses, 1, trucks, 2, cars, 3);
+	transport = new Truck();
 
-	cout << total << endl;
+	//cout << total << endl;
 
 	return 0;
 }
